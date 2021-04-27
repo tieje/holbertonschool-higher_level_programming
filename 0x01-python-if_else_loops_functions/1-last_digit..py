b6e803c39e22ec68a07cc957b215d5ad6a30673d
tieje <toj320@gmail.com>
2021-04-27 14:40:24 -0400

    fixed it

--- 0x01-python-if_else_loops_functions/1-last_digit..py
+++ 0x01-python-if_else_loops_functions/1-last_digit..py
@@ -4,13 +4,14 @@ number = random.randint(-10000, 10000)
 
 
 def greaterLessThanFive(n):
-    last_digit = n % 10
-    s_num = str(last_digit)
-    prefix = "Last digit of " + str(n) + " is " + s_num + " "
+    last_digit = abs(n) % 10
+    if n < 0:
+        last_digit = last_digit * -1
+    prefix = "Last digit of {:d} is {:d} ".format(n, last_digit)
     if (last_digit > 5):
         print(prefix + "and is greater than 5")
         return
-    if (last_digit < 5):
+    elif (last_digit < 5):
         print(prefix + "and is less than 6 and not 0")
         return
     print(prefix + "and is 0")
