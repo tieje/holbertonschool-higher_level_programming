e855a47cd4ec32fd7df74abcff09dce8e2f9b6b2
tieje <toj320@gmail.com>
2021-04-27 17:04:46 -0400

    fail safe

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10
+    last_digit = abs(number) % 10
     print("{:d}".format(last_digit), end="")
-    return
+    return last_digit
 
 
 """
-print_last_digit(-1024)
 print_last_digit(98)
+print_last_digit(0)
+r = print_last_digit(-1024)
+print(r)
 """
