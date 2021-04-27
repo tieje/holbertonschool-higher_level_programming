9214eb8f7cf164fe25b4456241bbdcd5407477c8
tieje <toj320@gmail.com>
2021-04-27 15:08:58 -0400

    trying

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-def zeroToNinetyNine():
-    for i in range(0, 99):
-        print(str(i), end=", ")
-    print("99")
-    return
-
-
-zeroToNinetyNine()
+for i in range(0, 99):
+    print("{:d}".format(i), end=", ")
+print("99")
