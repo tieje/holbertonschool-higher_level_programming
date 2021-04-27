a0adc0f23c9072758c453b4688f1ec0781b26269
tieje <toj320@gmail.com>
2021-04-27 14:45:21 -0400

    changed

--- 0x01-python-if_else_loops_functions/1-last_digit..py
+++ 0x01-python-if_else_loops_functions/1-last_digit..py
@@ -7,14 +7,14 @@ def greaterLessThanFive(n):
     last_digit = abs(n) % 10
     if n < 0:
         last_digit = last_digit * -1
-    prefix = "Last digit of {:d} is {:d} ".format(n, last_digit)
+    print("Last digit of {:d} is {:d} ".format(n, last_digit), end="")
     if (last_digit > 5):
-        print(prefix + "and is greater than 5")
+        print("and is greater than 5")
         return
-    elif (last_digit < 5):
-        print(prefix + "and is less than 6 and not 0")
+    elif (last_digit == 0):
+        print("and is 0")
         return
-    print(prefix + "and is 0")
+    print("and is less than 6 and not 0")
     return
 
 
