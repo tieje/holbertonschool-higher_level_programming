4eb836927ea74892f1d81331295d8d73bb0a70ae
tieje <toj320@gmail.com>
2021-04-27 16:29:46 -0400

    trying

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,11 +7,11 @@ def uppercase(str):
         if ordOf < 124 and ordOf > 96:
             printThis = chr(ordOf - 32)
         print("{}".format(printThis), end="")
-    print("")
+    print("".format())
     return
 
 
-"""
+
 uppercase("a string")
 uppercase("ThisstING")
-"""
+uppercase("98 Battery Street")
