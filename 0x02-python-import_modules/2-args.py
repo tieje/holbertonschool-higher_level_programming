d8cf4fd733b851ca3e53cd212f5ed0f9aec11f91
tieje <toj320@gmail.com>
2021-04-29 14:27:42 -0400

    forgot to add colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         suffix = "."
     if arglen == 2:
         arguments = "argument"
-    print("{} {}".format(arglen - 1, arguments))
+    print("{} {}{}".format(arglen - 1, arguments, suffix))
     if arglen > 1:
         for i in range(1, arglen):
             print("{}: {}".format(i, argv[i]))
