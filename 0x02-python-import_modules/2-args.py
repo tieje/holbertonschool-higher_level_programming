f2f84c59aa7353f3aebbab9a783d2d4dd1541943
tieje <toj320@gmail.com>
2021-04-29 14:25:01 -0400

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    arglen = len(argv)
+    arguments = "arguments"
+    suffix = ":"
+    if arglen == 1:
+        suffix = "."
+    if arglen == 2:
+        arguments = "argument"
+    print("{} {}".format(arglen - 1, arguments))
+    if arglen > 1:
+        for i in range(1, arglen):
+            print("{}: {}".format(i, argv[i]))
