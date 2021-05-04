f509b2865822bb097ca8d639421c6271bda1738a
tieje <toj320@gmail.com>
2021-05-04 16:22:04 -0400

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    truthy = []
+    for i in my_list:
+        if i % 2 == 0 and i != 0:
+            truthy.append(True)
+        else:
+            truthy.append(False)
+    return truthy
