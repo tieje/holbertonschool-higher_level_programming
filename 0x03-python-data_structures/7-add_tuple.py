15efb5653b0521ec6bdc2b3a77924c19eba8d821
tieje <toj320@gmail.com>
2021-05-04 16:00:56 -0400

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res = ()
+    while len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
+    while len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+    for x, y in zip(tuple_a, tuple_b):
+        res = res + (x + y,)
+    return res
