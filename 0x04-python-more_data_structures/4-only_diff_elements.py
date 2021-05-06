c4f8086ea6ea06cc59ba153babb03cb4eec852e5
tieje <toj320@gmail.com>
2021-05-06 14:18:56 -0400

    4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    
+    def findinset1(x): return x not in set_1
+    def findinset2(x): return x not in set_2
+    filterobj1 = list(filter(findinset1, set_2))
+    filterobj2 = list(filter(findinset2, set_1))
+    new_set = filterobj1 + filterobj2
+    return new_set
