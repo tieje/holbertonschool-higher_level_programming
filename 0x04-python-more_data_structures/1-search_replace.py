4585d869acd9f8c31bede9605ba641c6fdce27b6
tieje <toj320@gmail.com>
2021-05-06 13:19:04 -0400

    no lambdas today buddy

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+    def replacethis(x):
+        if x == search:
+            x = replace
+        return x
+    new_list = list(map(replacethis, my_list))
+    return new_list
