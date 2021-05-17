4bccdcb03511f269a0c61e1d14d9d64e61b1904a
tieje <toj320@gmail.com>
2021-05-17 13:49:17 -0400

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    real_print = 0
     for i in range(x):
         try:
-            print(my_list[i])
+            print(my_list[i], end='')
+            real_print += 1
         except:
             break
+    print('', end='\n')
+    return real_print
