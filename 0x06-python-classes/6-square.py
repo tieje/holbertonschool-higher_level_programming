9b21d75a6a1f273e447cd63950b2886d702874dd
tieje <toj320@gmail.com>
2021-05-19 09:14:05 -0400

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,12 +33,12 @@ class Square:
         """
         if type(value) != tuple or \
             len(value) > 2 or \
-            len(value) < 2 or \
-            type(value[0]) != int or \
-            type(value[1]) != int or \
-            value[0] < 0 or \
-            value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            len(value) < 2:
+                if type(value[0]) != int or \
+                type(value[1]) != int or \
+                value[0] < 0 or \
+                value[1] < 0:
+                    raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     @property
