cf7b618d70375e9c1517a9452836322f1ad8d29d
tieje <toj320@gmail.com>
2021-05-19 09:10:05 -0400

    there

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,13 @@ class Square:
         Args:
             value (tuple): value of tuples
         """
-        if type(value) != tuple or len(value) > 2 or len(value) < 2 or type(value[0]) != int or type(value[1]) != int or value[0] < 0 or value[1] < 0:
+        if type(value) != tuple or \
+            len(value) > 2 or \
+            len(value) < 2 or \
+            type(value[0]) != int or \
+            type(value[1]) != int or \
+            value[0] < 0 or \
+            value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
