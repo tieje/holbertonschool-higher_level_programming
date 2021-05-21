36f43bb36f09ef9d15384679323319d6304efd62
tieje <toj320@gmail.com>
2021-05-21 15:44:55 -0400

    gottem

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,14 +6,25 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-
+    """
+    Class for testing Max Integer function
+    """
     def test_return_max(self):
+        """
+        tests basic usage
+        """
         self.assertEqual(102, max_integer([1, 2, 100, 3, 102]))
 
     def test_return_none(self):
+        """
+        test return None when list is empty
+        """
         self.assertEqual(None, max_integer([]))
 
     def test_default_none(self):
+        """
+        test return None when input is empty
+        """
         self.assertEqual(None, max_integer())
 
 
