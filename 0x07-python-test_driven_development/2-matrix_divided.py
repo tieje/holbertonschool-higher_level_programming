30eda19595e72590f0e912a3d3b119ce24e93a52
tieje <toj320@gmail.com>
2021-05-22 11:38:23 -0400

    1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,14 +14,21 @@ def matrix_divided(matrix, div):
         new matrix
     """
     new_matrix = []
+    if matrix is None or div is None:
+        raise TypeError("div must be a number")
     # tests for div
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     # tests if the rows have the same length
-    if all([len(i) == len(matrix[0]) for i in matrix]) == False:
+    if all([len(i) == len(matrix[0]) for i in matrix]) is False:
         raise TypeError("Each row of the matrix must have the same size")
+    if div == float('inf'):
+        def Zeros(x): return 0
+        for i in matrix:
+            new_matrix.append(list(map(Zeros, i)))
+        return new_matrix
 
     def DivF(x):
         if type(x) != int and type(x) != float:
