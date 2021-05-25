857598e57413842cde980e42e21113acce116623
tieje <toj320@gmail.com>
2021-05-24 22:31:04 -0400

    wow I'm learning a lot..

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,7 +93,7 @@ class Rectangle:
         if rect_2.area() > rect_1.area():
             return rect_2
         return rect_1
-    
+
     @classmethod
     def square(cls, size=0):
         return Rectangle(size, size)
