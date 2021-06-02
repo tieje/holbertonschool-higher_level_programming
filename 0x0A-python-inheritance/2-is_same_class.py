ec8eafe9152ccbeb2b746654a6d944e767413594
tieje <toj320@gmail.com>
2021-06-02 19:35:19 -0400

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module is not special yet"""
+
+
+def is_same_class(obj, a_class):
+    """checks if the object is an instance of a class"""
+    return isinstance(obj, a_class)
