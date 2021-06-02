380ecfbd27d136d4fbe59787aad29e37179a27f3
tieje <toj320@gmail.com>
2021-06-02 19:58:13 -0400

    3 Pep8

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module is not special yet"""
+
+
 def is_kind_of_class(obj, a_class):
     """Check if the object is an object or instance of class"""
     return isinstance(obj, a_class)
