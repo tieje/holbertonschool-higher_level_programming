cc8af73a6a9d3caeb3878d42f455127fe7420a54
tieje <toj320@gmail.com>
2021-06-03 16:57:10 -0400

    added encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module reads a text file"""
+
+
 def read_file(filename=""):
     """This method reads a file."""
-    with open(filename, 'r') as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read())
         file.close()
