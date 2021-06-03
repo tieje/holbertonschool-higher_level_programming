0f4cf82cc43cfbc72edf8047bd4eee99963f1434
tieje <toj320@gmail.com>
2021-06-03 17:54:28 -0400

    comments for 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import json
 """Returns json repr of an object(string)"""
+import json
 
 
 def to_json_string(my_obj):
+    """Returns json repr of an object"""
     return json.dumps(my_obj)
