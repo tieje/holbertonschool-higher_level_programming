fd0ad979b8f631e821397128d0a9fe65e3b64c84
tieje <toj320@gmail.com>
2021-06-03 19:44:36 -0400

    8

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file using JSON repr"""
-    with open(filename, mode='w', encoding='utf_8') as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         json.dump(my_obj, file)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Return dictionary description with data structure
+"""
+def class_to_json(obj):
+    """
+    Return dictionary description with data structure
+    """
+    return obj.__dict__
