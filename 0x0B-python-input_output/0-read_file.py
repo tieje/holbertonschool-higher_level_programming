6f0e72908570d991a684b7c0e614b434eccfaf99
tieje <toj320@gmail.com>
2021-06-03 16:57:54 -0400

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """This method reads a file."""
-    with open(filename, mode="r", encoding="utf-8") as file:
+    with open(filename, mode="r", encoding="UTF8") as file:
         print(file.read())
         file.close()
