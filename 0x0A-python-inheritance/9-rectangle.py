dd155b9b86df6e550a28e1747dff7bc7c167a6f8
tieje <toj320@gmail.com>
2021-06-02 21:59:53 -0400

    9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Base Geometry"""
+
+    def area(self):
+        """Does nothing so far"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Integer validator"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Base Rectangle"""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        """Overwrites old area method"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[{}] {}/{}".format(__class__.__name__, self.__width, self.__height)
