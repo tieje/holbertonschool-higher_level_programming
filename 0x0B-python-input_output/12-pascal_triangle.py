6c43d04c73619f9810b4b0c78251972e358cf183
tieje <toj320@gmail.com>
2021-06-03 21:21:22 -0400

    12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Return a list of Integers representing Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    Return a list of Integers representing Pascal's triangle of n
+    """
+    result = []
+    for i in range(n):
+        result.append(list(str(11**i)))
+    return result
