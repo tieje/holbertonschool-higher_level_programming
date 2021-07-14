f2f333b3a3ac36f059f551756ad8a93789880fb4
tieje <toj320@gmail.com>
2021-07-14 16:20:32 -0400

    hm

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,10 +1,4 @@
 -- lists all cities
-SELECT
-    cities.id,
-    cities.name,
-    states.name
-FROM
-    cities,
-JOIN states
-    ON cities.state_id = state.id
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
