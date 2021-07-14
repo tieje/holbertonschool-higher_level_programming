e48aeee80e04ef4d1aae50b028042e0340b4f452
tieje <toj320@gmail.com>
2021-07-14 16:01:01 -0400

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,9 @@
 -- create table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL,
     name VARCHAR(256),
-    FOREIGN KEY REFERENCES states(id)
+    UNIQUE(id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
