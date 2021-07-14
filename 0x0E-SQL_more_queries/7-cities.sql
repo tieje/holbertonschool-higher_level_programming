47ad8ac0aa918c6b8c84cdfd151cd3cb48a48698
tieje <toj320@gmail.com>
2021-07-14 16:02:56 -0400

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- create database hbtn_0d_usa
 -- create table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL,
