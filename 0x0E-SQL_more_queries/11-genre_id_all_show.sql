5d7bdf5002e875968ae7d40a1f6c4ac52e21cd0c
tieje <toj320@gmail.com>
2021-07-14 16:31:55 -0400

    11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_show.sql
@@ -0,0 +1,7 @@
+-- list all shows in db
+SELECT
+    tv_shows.title,
+    tv_shows.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title DESC, tv_show_genres.genre_id ASC;
