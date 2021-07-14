4e270e2ed3932cca8402e189e5e0a9b503469cd8
tieje <toj320@gmail.com>
2021-07-14 17:09:05 -0400

    15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,4 @@
+-- list all comedy shows
+SELECT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy' ORDER BY tv_shows.title ASC;
