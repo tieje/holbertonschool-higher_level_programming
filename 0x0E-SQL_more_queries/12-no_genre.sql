563947b5d49235b97f3e0c7088baf45eb81f602a
tieje <toj320@gmail.com>
2021-07-14 16:44:39 -0400

    12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,6 @@
 -- list all shows without a genre linked
-SELECT
-    tv_shows.title,
-    tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show.genre_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
