2bcb0b3e606dcc7dccbc35dec4ce17ee763638b6
tieje <toj320@gmail.com>
2021-08-02 13:32:17 -0400

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-const convertToNumber = (arr) => {
-  let p_number = false;
-  if (arr.length === 3) {
-    p_number = Number(arr[2]);
-  }
-  if (isNaN(p_number)) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + p_number);
-  }
-};
-
-convertToNumber(process.argv);
+if (isNaN(Number(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
