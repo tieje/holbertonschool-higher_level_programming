f18a7b756038aa57d4b3214afc30b4f01680eae1
tieje <toj320@gmail.com>
2021-08-02 13:28:08 -0400

    4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,15 +1,2 @@
 #!/usr/bin/node
-
-const printIsArgs = (arr) => {
-  let ln = arr.length;
-  let first_arg = 'undefined', sec_arg = 'undefined';
-  if (ln >= 3) {
-    first_arg = arr[2];
-  }
-  if (ln === 4) {
-    sec_arg = arr[3];
-  }
-  console.log(first_arg + ' is ' + sec_arg);
-};
-
-printIsArgs(process.argv);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
