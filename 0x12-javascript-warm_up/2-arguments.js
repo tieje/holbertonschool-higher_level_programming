7d1b045cf4e883d26abe98a165f04c2fb395b5d5
tieje <toj320@gmail.com>
2021-08-02 13:21:20 -0400

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,12 @@
 import { argv } from 'process';
 
 const ArgsFound = (arg) => {
-  if (arg.length === 3) {
-    console.log('Argument found');
-  } else if (arg.length > 3) {
-    console.log('Arguments found');
-  } else {
+  if (arg.length === 2) {
     console.log('No argument');
+  } else if (arg.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
   }
 };
 
