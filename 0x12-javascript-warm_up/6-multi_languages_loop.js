a7387ddb1c76a3d872c1dc7bf3d4b24006e3b5a9
tieje <toj320@gmail.com>
2021-08-02 13:36:45 -0400

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const printThreeLines = () => {
-  let arr = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
-};
-printThreeLines();
+let arr = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
