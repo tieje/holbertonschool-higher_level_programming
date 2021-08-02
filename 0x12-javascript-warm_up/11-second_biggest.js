9bd05e1243ccb3b38b39c8a9c1b6bf3bd4fee925
tieje <toj320@gmail.com>
2021-08-02 15:54:34 -0400

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  process.argv.sort(function (a, b) { return b - a;});
+  process.argv.sort(function (a, b) {return b - a;});
   console.log(process.argv[3]);
 }
