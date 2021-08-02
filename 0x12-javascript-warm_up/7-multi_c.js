a5f245f30f0c596cd1d5b8cb9733221a3cb76a6d
tieje <toj320@gmail.com>
2021-08-02 13:43:38 -0400

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ if (arg.length < 3) {
   console.log('Missing number of occurrences');
 } else {
   if (Number(arg[2] > 0)) {
-    for (var i = 0; i < Number(arg[2]); i++) {
+    for (let i = 0; i < Number(arg[2]); i++) {
       console.log('C is fun');
     }
   }
