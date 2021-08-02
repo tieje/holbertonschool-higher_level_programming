6e4e817163b4fcb45ecd7348b668b2a44df32a76
tieje <toj320@gmail.com>
2021-08-02 15:46:01 -0400

    10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function getFactorio (a) {
+  const n = Number(a);
+  if (n === NaN || n === 1) {
+    return (1);
+  }
+  return n * getFactorio(n - 1);
+}
+console.log(getFactorio(process.argv[2]));
