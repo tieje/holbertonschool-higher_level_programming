3b1614e9199cdd28d3cd93402d4263ba3f47e7dd
tieje <toj320@gmail.com>
2021-08-02 13:43:02 -0400

    7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const printXTimes = (arg) => {
-  if (arg.length < 3) {
-    console.log('Missing number of occurrences');
-  } else {
+arg = process.argv;
+if (arg.length < 3) {
+  console.log('Missing number of occurrences');
+} else {
+  if (Number(arg[2] > 0)) {
     for (var i = 0; i < Number(arg[2]); i++) {
       console.log('C is fun');
     }
   }
-};
-
-printXTimes(process.argv);
+}
