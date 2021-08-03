d633121eb6a65793cb7281fd7b07f6b8dd1eb778
tieje <toj320@gmail.com>
2021-08-03 13:43:04 -0400

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
-#!/usr/local/bin/node
-module.exports =
-  class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  };
+  }
+}
+module.exports = Rectangle;
