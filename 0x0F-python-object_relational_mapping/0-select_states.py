17e5defa037c3081868ef84d443894a7d163237f
tieje <toj320@gmail.com>
2021-08-06 16:13:23 -0400

    1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+
+if __name__ == "__main__":
+    from MySQLdb import _mysql as my
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    db = my.connect(host="localhost", user=username,
+                    passwd=password, db=db_name)
+    db.query("""SELECT * FROM states ORDER BY states.id ASC;""")
+    rows = db.store_result()
+    for row in rows.fetch_row(0):
+        print(row)
