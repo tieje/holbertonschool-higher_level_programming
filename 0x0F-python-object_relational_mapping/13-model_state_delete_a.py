22ea59526a6ed0341882eaa4706f0662d76a3a1c
tieje <toj320@gmail.com>
2021-08-07 13:09:44 -0400

    13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -20,7 +20,6 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(engine)
     session = Session()
-    query = session.query(State).filter(State.name.like('%a%'))
-    session.delete(query)
+    query = session.query(State).filter(State.name.like('%a%')).delete()
     session.commit()
     session.close()
