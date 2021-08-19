55c60795a8fb98c9821f606c03956a1d35b23a39
tieje <toj320@gmail.com>
2021-08-19 18:58:52 -0400

    hm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,5 +5,6 @@
 def find_peak(list_of_integers):
     '''finds the greatest number from the list'''
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return(list_of_integers[-1])
     return None
