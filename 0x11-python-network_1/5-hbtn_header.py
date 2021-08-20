939f47fa0ba930d72e38d857acdfb952e11f165a
tieje <toj320@gmail.com>
2021-08-20 19:04:35 -0400

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+'''Get response of header'''
 
+from requests import get
+from sys import argv as av
 if __name__ == "__main__":
+    print(get(av[1]).headers.get("X-Request-Id"))
