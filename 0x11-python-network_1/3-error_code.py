211b3195b384537bbf3f8685b08d457387374a2b
tieje <toj320@gmail.com>
2021-08-20 17:37:37 -0400

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+'''
+Script that accepts URL and display body
+'''
+import urllib.error as ue
+import urllib.request as ur
+from sys import argv as av
 
 if __name__ == "__main__":
+    url_request = av[1]
+    try:
+        with ur.urlopen(url_request) as response:
+            print(response.read().decode('utf-8'))
+    except ue.HTTPError as err:
+        print("Error code: {}".format(err.code))
