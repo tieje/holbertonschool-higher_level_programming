a0a77a35dffa50b75dd74928401e2079d6e85eb9
tieje <toj320@gmail.com>
2021-08-20 16:26:58 -0400

    switch comments and import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import urllib.request as ur
-import sys
 '''
 Script that takes in a URL, sends a
 request to the URL and displays 
 the value of the X-Request-Id variable
 found in the header of the response
 '''
+import urllib.request as ur
+import sys
 if __name__ == "__main__":
     url_request = sys.argv[1]
     with ur.urlopen(url_request) as req:
