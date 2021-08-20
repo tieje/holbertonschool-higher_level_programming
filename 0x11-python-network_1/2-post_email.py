4338323e24f692f2fc995b7507a18c03978850ed
tieje <toj320@gmail.com>
2021-08-20 17:19:51 -0400

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+'''
+Send Post request with email as parameter
+'''
+import urllib.request as uresp
+import urllib.parse as upar
 
+from sys import argv
 if __name__ == "__main__":
+    url_request = argv[1]
+    email_input = argv[2]
+    data = upar.urlencode({'email': email_input})
+    data = data.encode('utf-8')
+    with uresp.urlopen(url_request, data=data) as response:
+        print(response.read())
