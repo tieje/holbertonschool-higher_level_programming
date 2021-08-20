ad1c6b26c0c80e2ea0d85fec3f7529d0636ea849
tieje <toj320@gmail.com>
2021-08-20 19:01:43 -0400

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     response = get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("]t- content: {}".format(response.text))
+    print("\t- content: {}".format(response.text))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+from requests import post
+from sys import argv as av
+import seleneium
 
 if __name__ == "__main__":
+    data = {"email": av[2]}
+    print(post(argv[1], data=data).text)
