1855279047cee6efff3b67fb71978b754171a54e
tieje <toj320@gmail.com>
2021-08-20 20:50:17 -0400

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,7 @@
 from requests import get
 from sys import argv as av
 if __name__ == "__main__":
-    response = get(av[2])
+    response = get(av[1])
     status = response.status_code
     if status >= 400:
         print("Error code: {}".format(status))
