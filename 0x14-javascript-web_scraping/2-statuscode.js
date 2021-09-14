536f9c46ed1f71fddebc11254d577ea951db9d0d
tieje <toj320@gmail.com>
2021-09-14 15:34:10 -0400

    ignoring node modules

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
+// #!/usr/bin/node
+// request status code
+const rq = require('request');
+rq(process.argv[2], (err, response, body) => {
+  console.log('code:', response.statusCode);
+  if (err) {
+    console.log(err);
+  }
+});
