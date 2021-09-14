6003ed41de6152f5c5a82c2beb9bd46efab05632
tieje <toj320@gmail.com>
2021-09-14 14:54:43 -0400

    way overboard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,24 +3,30 @@
 // Reads and prints the content of the file
 
 const fs = require('fs');
-const editFile = process.argv[2];
-const strs = 'C is super fun!';
+const readThisFile = process.argv[2];
+fs.readFile(readThisFile, 'utf8', (err, data) => {
+  if (err) return (console.log(err));
+  console.log(data);
+});
+/*
+const strs = 'C is super fun!'
 const writeOptions = {
   encoding: 'utf8',
   flag: 'w'
-};
+}
 fs.writeFile(editFile, strs, writeOptions, (err) => {
   if (err) {
-    return err;
+    return err
   }
-});
+})
 const readOptions = {
   encoding: 'utf8',
   flag: 'r'
-};
+}
 fs.readFile(editFile, readOptions, (err, data) => {
   if (err) {
-    return err;
+    return err
   }
-  console.log(data);
-});
+  console.log(data)
+})
+*/
