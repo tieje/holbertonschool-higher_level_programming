dbe981889e91b39fca9e2c621eb8025548071caa
tieje <toj320@gmail.com>
2021-09-14 15:34:32 -0400

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
-#!/usr/local/bin/node
-// #!/usr/bin/node
+#!/usr/bin/node
+// #!/usr/local/bin/node
 // request status code
 const rq = require('request');
 rq(process.argv[2], (err, response, body) => {
