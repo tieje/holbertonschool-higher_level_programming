dce36938fbe151a3db3ed51afa096e9a6b52a508
tieje <toj320@gmail.com>
2021-09-14 15:15:48 -0400

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
+// #!/usr/bin/node
+// Reads and prints the content of the file
+
+const fs = require('fs');
+const wF = process.argv[2];
+const wString = process.argv[3];
+const writeOptions = {
+  encoding: 'utf8',
+  flag: 'w'
+};
+fs.writeFile(wF, wString, writeOptions, (err) => {
+  if (err) {
+    return err;
+  }
+});
